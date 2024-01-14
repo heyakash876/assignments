f4255e5ac26ee644a284181b6b91e77833472ef9
heyakash876 <119721777+heyakash876@users.noreply.github.com>
2024-01-14 20:13:53 +0530

    Create TokenA.sol

---
+++ Assignment3/TokenA.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenA is IERC20, ERC20Burnable, ERC20Pausable, Ownable {
+    constructor(string memory name, string memory symbol)
+        ERC20(name, symbol)
+    {}
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+}
